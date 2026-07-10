953916f22b0dfd370fe765f008797d63b3669c15
klavik <40463587+klavik@users.noreply.github.com>
2026-07-10 12:43:50 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+window.APP_CONFIG = {
+  // Supabase Dashboard → Project Settings → API
+  supabaseUrl: "https://bhtotueoyxprvzwbjrkd.supabase.co",
+
+  // Используйте Publishable key. Никогда не помещайте сюда service_role key.
+  supabasePublishableKey: "sb_publishable_tN-ME_eGEnfiM2OTon-d0g_-_6RGpp7"
+};
